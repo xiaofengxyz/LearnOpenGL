87ce719377ab05ab2b9b33b48ebb804c63bfb498
bibaofeng <baofeng.bi@pplingo.com>
2023-09-22 19:02:24 +0800

    fix 摄像机移动

--- TestOpenGL/TestOpenGL/main.cpp
+++ TestOpenGL/TestOpenGL/main.cpp
@@ -17,6 +17,10 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+
 int main()
 {
     // glfw: initialize and configure
@@ -242,7 +246,20 @@ int main()
 
           glDrawArrays(GL_TRIANGLES, 0, 36);
         }
-        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+//        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+//        view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
+//                   glm::vec3(0.0f, 0.0f, 0.0f),
+//                   glm::vec3(0.0f, 1.0f, 0.0f));
+        
+//        float radius = 10.0f;
+//        float camX = sin(glfwGetTime()) * radius;
+//        float camZ = cos(glfwGetTime()) * radius;
+//        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+        
+
+        
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         // retrieve the matrix uniform locations
         unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
@@ -282,6 +299,15 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    float cameraSpeed = 0.05f; // adjust accordingly
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+       cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+       cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+       cameraPos -= glm::cross(cameraFront, cameraUp) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+       cameraPos += glm::cross(cameraFront, cameraUp) * cameraSpeed;
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
